3f82ab289767b78568c5cc4c135f1a82da353b7c
Chris Adkins <adkinscda@gmail.com>
2020-01-30 13:45:37 -0800

    Added ReviewQuestions.txt for Week 1

--- Week 1/Test.cpp
+++ Week 1/Test.cpp
@@ -9,9 +9,22 @@
 */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    cout << "Hello World!";
+    int array1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int array2[10];
+    vector<int> vec1(10, 100);
+    vector<int> vec2(vec1);
+    cout << vec1[4] << "\n" << vec2[4] << endl;
+
+    for (int i = 0; i < 10; i++) {
+        array2[i] = array1[i];
+    }
+
+    for (int i = 0; i < 10; i++) {
+        cout << array2[i] << ", ";
+    }
 }
