56f007bbc48063096c831134247e36cbe393e276
ideobyte <adkinscda@gmail.com>
2020-04-05 12:17:56 -0700

    Updated Animal Lab

--- Week 7/Lab/Animals/Main.cpp
+++ Week 7/Lab/Animals/Main.cpp
@@ -13,6 +13,18 @@
         5. Override the animal class's functions in the derivitive classes.
         6. Display the various animals and their respective functions to the user.
 
+        Questions:
+            1. The base class is called Animal.
+            2. The derived classes are Bird and Dog.
+            3. No, Animal does not have a parent class.
+            4. showInfo() is overwritten, makeNoise() isoverwritten, sleep() is not.
+            5. 
+                a. If the name variable were made protected then it could be freely accessed
+                   by the child classes. No need for getters and setters. 
+               
+                b. If the Animal class's access modifier were instead changed, it would
+                   not make the name variable accessible to the child classes.
+
     Classes Needed:
         <iostream> - For printing to the user.
         "Animal.h" - For the base class.
