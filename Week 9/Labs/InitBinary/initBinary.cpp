1a10675ea2aefee945dc4d2fb1c3362a00779e9f
ideobyte <adkinscda@gmail.com>
2020-04-19 01:08:58 -0700

    Finished Week 9 Labs

--- Week 9/Labs/InitBinary/initBinary.cpp
+++ Week 9/Labs/InitBinary/initBinary.cpp
@@ -1,3 +1,15 @@
+/*  initBinary.cpp
+    Author: Chris Adkins
+    Module: Week 9
+    Project: Lab 1
+    Problem Statement: This program write a series of ints and doubles to abinary file.
+
+    Classes Needed:
+        <iostream> - For printing to the user.
+        <fstream>  - For opening file input / output streams.
+        <math.h>   - For the sqrt() function.
+*/
+
 #include <iostream>
 #include <fstream>
 #include <math.h>
@@ -5,22 +17,40 @@
 using namespace std;
 
 int main() {
-    const string fileName = "array.dat";
-    ofstream outFile(fileName, ios::out | ios::binary);
+    const string fileName = "data.dat";
+    fstream dataFile(fileName, ios::out | ios::binary); // File stream set up for output to a binary file.
 
-    if (!outFile) {
+    int nums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    if (!dataFile) { // If there is a problem opening / creating the file.
         cout << "Error opening file: " << fileName;
         exit(1);
     }
 
-    for (int i = 1; i <= 10; i++) {
-        outFile.write(reinterpret_cast<char *>(&i), sizeof(int));
+    for (int i = 0; i < 10; i++) { // Writing to the dat file.
+        dataFile.write(reinterpret_cast<char *>(&nums[i]), sizeof(int));
     }
 
-    for (int i = 1; i <= 10; i++) {
-        double square = sqrt(i);
-        cout << square << endl;
-        outFile.write(reinterpret_cast<char *>(&square), sizeof(double));
+    for (int i = 0; i < 10; i++) { // Writing to the dat file.
+        double num = sqrt(nums[i]);
+        dataFile.write(reinterpret_cast<char *>(&num), sizeof(double));
     }
-    outFile.close();
+
+    dataFile.close(); // Closing the output stream to the file.
+    dataFile.open(fileName, ios::binary | ios::in); // Opening the file as an input stream.
+    cout << "Reading the first 10 values from the file:\n";
+
+    // I read from the file so I can verify the writes without using a hex editor.
+    for (int i = 0; i < 10; i++) {
+        int data;
+        dataFile.read(reinterpret_cast<char *>(&data), sizeof(int));
+        cout << data << endl;
+    }
+
+    cout << "\nReading the last 10 values from the file:\n";
+    for (int i = 0; i < 10; i++) {
+        double data;
+        dataFile.read(reinterpret_cast<char *>(&data), sizeof(double));
+        cout << data << endl;
+    }
 }
--- Week 9/Labs/ReadBinary/readBinary.cpp
+++ Week 9/Labs/ReadBinary/readBinary.cpp
@@ -1,3 +1,15 @@
+/*  readBinary.cpp
+    Author: Chris Adkins
+    Module: Week 9
+    Project: Lab 2
+    Problem Statement: This program reads ints and doubles from a binary file, multiplies the values, and replaces them.
+
+    Classes Needed:
+        <iostream> - For printing to the user.
+        <fstream>  - For opening file input / output streams.
+        <math.h>   - For the round() function.
+*/
+
 #include <iostream>
 #include <fstream>
 #include <math.h>
@@ -5,7 +17,7 @@
 using namespace std;
 
 int main() {
-    const string fileName("array.dat");
+    const string fileName("data.dat");
     double multiplier;
 
     fstream dataFile(fileName, ios::binary | ios::in | ios::out);
@@ -17,7 +29,8 @@ int main() {
     cout << "Enter a multiplier: ";
     cin >> multiplier;
 
-    for (int i = 0; i < 20; i++) {
+    cout << "Multiplying the 10 ints in the file by " << multiplier << "...\n";
+    for (int i = 0; i < 10; i++) { // Looping through the first 10 elements in the file.
         int data;
         long position = dataFile.tellg();
         dataFile.read(reinterpret_cast <char *>(&data), sizeof(int));
@@ -26,5 +39,17 @@ int main() {
         cout << data << endl;
         dataFile.seekp(position, ios::beg); // Writing the updated number.
         dataFile.write(reinterpret_cast <char *>(&data), sizeof(int));
+    }
+
+    cout << "\nMultiplying the 10 doubles in the file by " << multiplier << "...\n";
+    for (int i = 0; i < 10; i++) { // Looping through the next 10 elements in the file.
+        double data;
+        long position = dataFile.tellg();
+        dataFile.read(reinterpret_cast <char *>(&data), sizeof(double));
+        cout << data << " -> ";
+        data = data * multiplier;
+        cout << data << endl;
+        dataFile.seekp(position, ios::beg); // Writing the updated number.
+        dataFile.write(reinterpret_cast <char *>(&data), sizeof(double));
     }
 }
