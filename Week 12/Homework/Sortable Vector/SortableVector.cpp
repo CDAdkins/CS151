511659a874f574f558fdb2568b0a3ff090a35455
Chris Adkins <adkinscda@gmail.com>
2020-05-11 21:29:44 -0700

    Finished Week 12

--- Week 12/Homework/Sortable Vector/SortableVector.cpp
+++ Week 12/Homework/Sortable Vector/SortableVector.cpp
@@ -1,50 +1,103 @@
-/*  SortableVector.cpp
+/*  SimpleVector.cpp
     Author: Chris Adkins
     Module: Homework 13
-    Project: 9
-    Problem Statement: This program modifies the SimpleVector class to include a sort function.
+    Project: 7
+    Problem Statement: This program modifies the SimpleVector class to include a push_back and pop_back function.
 
     Algorithm:
-        1. Add the sort function.
-            a. 
+        1. Add the sort function().
+            a. Create two for loops, one inside the other.
+            b. If the value on the left is higher than the value on the right, swap them.
+            c. Print the results to the user.
 
     Classes Needed:
         <iostream> - For printing to the user.
 */
 
 #include <iostream>
-#include "SimpleVector.h"
 using namespace std;
 
 template <class T>
-class SortableVector : public SimpleVector<T> {
-    private:
-    public:
-    /* SortableVector(int i) : SimpleVector<T>(i) {
-        // Passing the constructor to our base class.
-    } */
-
-    SortableVector(int s) {
-        SimpleVector<T>::arraySize = s ;
-        SimpleVector<T>::aptr = new T [s] ;
-
-        for (int count = 0 ; count < SimpleVector<T>::arraySize ; count++)
-        *(SimpleVector<T>::aptr + count) = 0;
-        
-    }
-
-    void print() {
-        for (int i = 0; i < SimpleVector<T>::arraySize; i++) {
-            cout << SimpleVector<T>::aptr[i];
-            i == SimpleVector<T>::arraySize ? cout << "\n" : cout << ", ";
+class SortableVector {
+    T *aptr;
+    int arraySize;
+public:
+    SortableVector(int num) { // Constructor.
+        arraySize = num;
+        aptr = new T[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            aptr[i] = T();
         }
     }
 
-    void sort() {
-        for (int i = 0; i < SimpleVector<T>::arraySize; i++) {
-            for (int x = 0; x < SimpleVector<T>::arraySize; x++) {
-                if (SimpleVector<T>::aptr[x] > SimpleVector<T>::aptr[x + 1] && x + 1 <= SimpleVector<T>::arraySize) {
-                    swap(SimpleVector<T>::aptr[x], SimpleVector<T>::aptr[x + 1]);
+    SortableVector(const SortableVector &obj) { // Copy Constructor.
+        arraySize = obj.arraySize;
+        aptr = new T[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            aptr[i] = obj[i];
+        }
+    }
+
+    ~SortableVector() { // Destructor.
+        if (arraySize > 0) {
+            delete [] aptr;
+        }
+    }
+
+    int size() const {
+        return arraySize;
+    }
+
+    T &operator[](int num) { // Overloaded [] operator.
+        if (num < 0 || num >= arraySize) {
+            throw string("Index Out of Bounds Error.\n");
+        }
+        return aptr[num];
+    }
+
+    void print() const {
+        for (int i = 0; i < arraySize; i++) {
+            cout << aptr[i];
+            i == (arraySize - 1) ? cout << "\n" : cout << ", "; // Print a comma unless it's the final element.
+        }
+    }
+
+    void push_back(T val) {
+        T *tempPtr = new T [arraySize + 1];
+        for (int i = 0; i < arraySize; i++) { // Copying the old array into the new array.
+            tempPtr[i] = aptr[i];
+        }
+        tempPtr[arraySize] = val; // Placing the value at the end of the new array.
+
+        if (arraySize > 0) // Deleting the old array.
+            delete [] aptr;
+
+        aptr = tempPtr; // Assigning aptr to the new array.
+        arraySize++; // Increment the array size.
+    }
+
+    T pop_back() {
+        if (arraySize == 0)
+            throw string("Vector is empty.\n");
+        else {
+            T lastValue = aptr[arraySize - 1]; // Hold the last value.
+            T *tempPtr = new T [arraySize - 1]; // Create a new array, one element smaller.
+            for (int i = 0; i < arraySize - 1; i++) {
+                tempPtr[i] = aptr[i];
+            }
+
+            delete [] aptr; // Delete the old array.
+            aptr = tempPtr; // Point aptr to the new array.
+            arraySize--; // Decrement the array size.
+            return lastValue; // Return the last value from the old array.
+        }
+    }
+
+    void sort() { // Bubble Sort
+        for (int i = 0; i < SortableVector<T>::arraySize; i++) {
+            for (int x = 0; x < SortableVector<T>::arraySize; x++) {
+                if (SortableVector<T>::aptr[x] > SortableVector<T>::aptr[x + 1] && x + 1 <= SortableVector<T>::arraySize) {
+                    swap(SortableVector<T>::aptr[x], SortableVector<T>::aptr[x + 1]);
                 }
             }
         }
